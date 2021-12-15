f6e933aa8f5a7d158dc86a933b82c8f7fe8f4932
Alec Musasa <alecmus@live.com>
2021-12-15 02:21:18 +0200

    Added basic png and ico resources.

---
+++ resource.h
@@ -0,0 +1,36 @@
+/*
+** MIT License
+**
+** Copyright(c) 2021 Alec Musasa
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this softwareand associated documentation files(the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions :
+**
+** The above copyright noticeand this permission notice shall be included in all
+** copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
+*/
+
+#pragma once
+
+#define ico_resource	100
+
+#define png_error		110
+#define png_settings	111
+#define png_back_light	112
+#define png_back_dark	113
+#define png_help		114
+#define png_updates		115
+#define splash_image_128	116
+#define splash_image_256	117
